fdef57b70d05bb2a7a69362510e76794b4c5ff25
julio-fontes <julio.fontes@trustly.com>
2025-10-17 07:27:09 -0300

    Initialize project

---
+++ src/main/java/com/jgfontes/recepy/RecepyApplication.java
@@ -0,0 +1,13 @@
+package com.jgfontes.recepy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgfontes/recepy/RecepyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgfontes.recepy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
